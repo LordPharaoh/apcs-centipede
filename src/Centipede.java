1ccea4c8e4de126bd011509eba2be010e1522f55
Conor Gomes <cg2895@pleasantonusd.net>
2017-02-08 14:56:12 -0800

    Start of Centipede class

---
+++ src/Centipede.java
@@ -0,0 +1,30 @@
+
+public class Centipede implements MovingObject, Drawable {
+
+	public Centipede(double x, double y, double wid, double ht, double health) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void move() {
+	
+	}
+	
+	public void getLocation() {
+		
+	}
+	
+	public void detectCollision() {
+		
+	}
+	
+	public void handleCollision() {
+		
+	}
+	
+	public void draw() {
+		
+	}
+}
+
+
