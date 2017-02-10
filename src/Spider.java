ec06bbbe6e62142fc5371b80a5ed5fba34e4ecce
Susan <susan.qing.chen@gmail.com>
2017-02-10 14:43:22 -0800

    added spider class

---
+++ src/Spider.java
@@ -0,0 +1,37 @@
+
+public class Spider implements MovingObject, Drawable{
+	
+	
+	
+	
+	public Spider (){
+		
+	}
+
+
+
+	@Override
+	public void move() {
+		/**
+		 * Moves randomly within a certain area
+		 */
+		//TODO implement
+	}
+	
+	public void handleCollision() {
+		/**
+		 * 
+		 */
+
+	}
+	
+	
+	@Override
+	public void draw() {
+		/**
+		 * 
+		 */
+
+	}
+
+}
