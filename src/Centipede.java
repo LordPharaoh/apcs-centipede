d1b5dcce2b84a740697bbc1e764228020bff2d7a
Varun <vi.mail@protonmail.ch>
2017-02-15 14:54:46 -0800

    Added placeholder draw and vector constructor

--- src/Centipede.java
+++ src/Centipede.java
@@ -1,13 +1,15 @@
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
 
 public class Centipede implements MovingObject, Drawable {
 
-	public Centipede(double x, double y, double wid, double ht, double health) {
-		// TODO Auto-generated constructor stub
+	private Vector location;
+	//TODO support scaling
+	public Centipede(Vector v) {
+		location = v;
 	}
-
 	@Override
 	public void move() {
 	
@@ -15,8 +17,9 @@ public class Centipede implements MovingObject, Drawable {
 	
 	@Override
 	public void draw(Graphics g) {
-		// TODO Auto-generated method stub
-		
+		// TODO placeholder draw function
+		g.setColor(new Color(0, 255, 0));
+		g.drawOval(this.location.x, this.location.y, 10, 10);
 	}
 
 	@Override
@@ -28,7 +31,7 @@ public class Centipede implements MovingObject, Drawable {
 	@Override
 	public Vector getLocation() {
 		// TODO Auto-generated method stub
-		return null;
+		return location;
 	}
 
 	@Override
@@ -39,7 +42,7 @@ public class Centipede implements MovingObject, Drawable {
 
 	@Override
 	public void handleCollision(MovingObject m) {
-		// TODO Auto-generated method stub
+		// TODO implement
 		
 	}
 }
