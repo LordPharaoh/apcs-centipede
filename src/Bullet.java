0989bf80f95c2c34a4141c2e463e8c82c995cbf3
Yiyun Gong <yvonnegong226@gmail.com>
2017-02-21 14:47:38 -0800

    Ad Bullet class

---
+++ src/Bullet.java
@@ -0,0 +1,52 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+
+public abstract class Bullet implements MovingObject, Drawable {
+	//TODO actual values
+	private int x, y;
+	public static final int BULLET_WIDTH = 2;
+	public static final int BULLET_HEIGHT = 2;
+	protected int bulletSpeed = 3;
+	private boolean hitObj;
+	private Vector location;
+	
+	public Bullet(int x, int y){
+		this.x = x;
+		this.y = y;
+		
+	}
+	
+
+	@Override
+	public void move() {
+		while(hitObj==false){
+			y+=bulletSpeed;
+		}
+		
+	}
+	@Override
+	public void handleCollision(MovingObject m){
+		
+	}
+
+	@Override
+	public void draw(Graphics g) {
+		g.setColor(Color.YELLOW);
+		g.fillRoundRect(x, y, (int)this.BULLET_WIDTH, (int)this.BULLET_HEIGHT, 1, 1);
+		
+	}
+	@Override
+	public Vector getLocation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public boolean collision(Rectangle r) {
+		// TODO Auto-generated method stub
+		return hitObj;
+	}
+
+}
