edcfa6233221c38b7350829b4298049d2b70b9a2
yvonn <yvonnegong226@gmail.com>
2017-02-27 10:52:44 -0800

    new Bullet class

--- src/Bullet.java
+++ src/Bullet.java
@@ -3,50 +3,64 @@ import java.awt.Graphics;
 import java.awt.Rectangle;
 
 
-public abstract class Bullet implements MovingObject, Drawable {
+public class Bullet implements MovingObject, Drawable{
 	//TODO actual values
 	private int x, y;
-	public static final int BULLET_WIDTH = 2;
-	public static final int BULLET_HEIGHT = 2;
-	protected int bulletSpeed = 3;
-	private boolean hitObj;
+	public static final int BULLET_WIDTH = 10;
+	public static final int BULLET_HEIGHT = 20;
+	protected int bulletSpeed = 5;
 	private Vector location;
+	public int Wall = 1000;
+
 	
-	public Bullet(int x, int y){
-		this.x = x;
-		this.y = y;
-		
+	public Bullet(Vector v){
+		location = v;
 	}
 	
 
 	@Override
 	public void move() {
-		while(hitObj==false){
-			y+=bulletSpeed;
-		}
-		
+		x = location.x;
+		y = location.y;
+		if(y>0 && y<Wall){
+			y=y-bulletSpeed;//draw(null);
+			location = new Vector(x, y);
+			}
 	}
+	
 	@Override
 	public void handleCollision(MovingObject m){
-		
+		if(m instanceof Mushroom || m instanceof Spider || m instanceof MovingObject) bulletSpeed =0;
+		else return;
 	}
 
 	@Override
 	public void draw(Graphics g) {
-		g.setColor(Color.YELLOW);
-		g.fillRoundRect(x, y, (int)this.BULLET_WIDTH, (int)this.BULLET_HEIGHT, 1, 1);
-		
+		g.setColor(Color.RED);
+		g.fillRoundRect(location.x, location.y, (int)this.BULLET_WIDTH, (int)this.BULLET_HEIGHT, 10, 10);
 	}
 	@Override
+	
 	public Vector getLocation() {
 		// TODO Auto-generated method stub
-		return null;
+		return location;
 	}
 	
 	@Override
 	public boolean collision(Rectangle r) {
 		// TODO Auto-generated method stub
-		return hitObj;
+		return false;
+	}
+
+
+	public void setLocation(Vector location) {
+	}
+
+
+	@Override
+	public Rectangle getBoundingRect() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
