4814156bc6ac578b3c9398f7639ab5d0dc8644f3
Varun Iyer <vi.mail@protonmail.ch>
2017-02-28 14:53:24 -0800

    New game if you kill all centipedes

--- src/CentipedeGameMap.java
+++ src/CentipedeGameMap.java
@@ -8,15 +8,15 @@ public class CentipedeGameMap extends GameMap {
 	public static double scale = 1;
 
 	Player p;
+	int level;
 	
 	public CentipedeGameMap(Dimension dim) {
 		//TODO open with dimensions
-		makeMushrooms(20);
-		makeCentipede(20, 600);
-		Spider s = new Spider(new Vector(0, MovingObjectsGameLauncher.DEFAULT_SIZE * 3 / 4), this);
+		level = 1;
+		makeGame(level);
 		p = new Player(new Vector((int)(MovingObjectsGameLauncher.DEFAULT_SIZE * .5), MovingObjectsGameLauncher.DEFAULT_SIZE - 100), this);
 		startGame();
-		add(s);
+
 		add(p);
 	}
 
@@ -39,7 +39,12 @@ public class CentipedeGameMap extends GameMap {
 		}
 	}
 		
-
+	public void makeGame(int level) {
+		makeMushrooms(50);
+		makeCentipede(level * 20, (int) (Math.random() * MovingObjectsGameLauncher.DEFAULT_SIZE));
+		Spider s = new Spider(new Vector(0, MovingObjectsGameLauncher.DEFAULT_SIZE * 3 / 4), this);
+		add(s);
+	}
 	public CentipedeGameMap() {
 		// TODO Auto-generated constructor stub
 	}
@@ -67,9 +72,22 @@ public class CentipedeGameMap extends GameMap {
 			movers.get(i).move();
 		}
 	}
+	
+	private int numCentipedes() {
+		int tot = 0;
+		for(int i = 0; i < movers.size(); i++) {
+			if (movers.get(i) instanceof Centipede) tot++;
+		}
+		return tot;
+	}
 
 	@Override
 	public void tick() {
+		if (numCentipedes() == 0){
+			makeGame(level);
+			level++;
+		}
+		
 		updateLists();
 		if(gameOver) return;
 		collisions();
