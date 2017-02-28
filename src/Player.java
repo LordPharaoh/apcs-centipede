0e1245496058a0d6549b2c030148dbca0f3ad4ee
Susan <susan.qing.chen@gmail.com>
2017-02-28 14:36:25 -0800

    restricted player movement

--- src/Player.java
+++ src/Player.java
@@ -9,47 +9,68 @@ import javax.swing.KeyStroke;
 
 public class Player implements MovingObject, Drawable{
 
-	public static final int WIDTH = 100;
-	public static final int HEIGHT = 50;
-	public static final int SPEED = 10;
+	public static final int WIDTH = 10;
+	public static final int HEIGHT = 10;
+	public static final int SPEED = 20;
 	private Vector location;
-	
-	
+
+
 	public Player (Vector v){
 		// TODO
 		location = v;
 
-		
-		
+
+
 	}
-	
-	
-	
-	
+
+
+
+
 	@Override
 	public void draw(Graphics g) {
 		// TODO Auto-generated method stub
 		g.setColor(new Color(255, 0, 0));
 		g.drawOval(this.location.x, this.location.y, WIDTH, HEIGHT);
-		
+
 	}
 
-	
+
 	public void move(String s) {
-		if (s.equalsIgnoreCase("left")){
-			location.x -= SPEED;
-		}
-		else if (s.equalsIgnoreCase("right")){
-			location.x += SPEED;
-		}
-		else if (s.equalsIgnoreCase("up")){
-			location.y -= SPEED;
-		}
-		else if (s.equalsIgnoreCase("down")){
-			location.y += SPEED;
-		}
 
+		if (s.equalsIgnoreCase("left")){
+			if (location.x - SPEED <= 0){
+				location.x = 0;
+			}
+			else{
+				location.x -= SPEED;
+			}
+		}
+		if (s.equalsIgnoreCase("right")){
+			if (location.x + SPEED >= MovingObjectsGameLauncher.DEFAULT_SIZE){
+				location.x = MovingObjectsGameLauncher.DEFAULT_SIZE;
+			}
+			else{
+				location.x += SPEED;
+			}
+		}
+		if (s.equalsIgnoreCase("up")){
+			if (location.y - SPEED <= 600){
+				location.y = 600;
+			}
+			else{
+				location.y -= SPEED;
+			}
+		}
+		if (s.equalsIgnoreCase("down")){
+			if (location.y + SPEED >= MovingObjectsGameLauncher.DEFAULT_SIZE){
+				location.y = MovingObjectsGameLauncher.DEFAULT_SIZE;
+			}
+			else{
+				location.y += SPEED;
+			}
+		}
 	}
+
 
 	@Override
 	public Rectangle getBoundingRect() {
@@ -71,9 +92,9 @@ public class Player implements MovingObject, Drawable{
 		 * cannot go through mushrooms
 		 * next health when hitting spider or centipede
 		 */
-		
-		
-		
+
+
+
 	}
 
 
@@ -82,7 +103,7 @@ public class Player implements MovingObject, Drawable{
 	@Override
 	public void move() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 
@@ -94,7 +115,7 @@ public class Player implements MovingObject, Drawable{
 		return (getBoundingRect()).intersects(r.getBoundingRect());
 	}
 
-	
-	
-	
+
+
+
 }
